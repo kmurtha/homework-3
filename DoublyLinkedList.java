cff0aa4d20cd2905a106555542fbb62228b37a24
kmurtha <kmurtha2@albany.edu>
2016-10-09 19:27:03 -0400

    Create DoublyLinkedList.java

---
+++ DoublyLinkedList.java
@@ -0,0 +1,34 @@
+import java.until.Scanner;
+/* class node */
+
+class Node
+{
+  protected int data;
+  protected Node next, prev;
+  
+  /* Constructor */
+  
+  public Node()
+  {
+    next = null;
+    prev = null;
+    data = 0;
+  }
+  
+  /*Constructor*/
+  
+  public Node(int d, Node d, Node p)
+    
+  {
+    data = d;
+    
+    next = n;
+    
+    prev = p;
+  }
+  
+  /*Function to set link to next node*/
+  
+  public void setLinkNext (Node n)
+  {
+    
